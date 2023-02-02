71990aa3e342bd503ad4becbb99a3b97f8138489
pratiksha <pratikshabarge26@gmail.com>
2023-02-02 16:16:57 +0530

    binary search commited

---
+++ BinarySearch.java
@@ -0,0 +1,32 @@
+class BinarySearch
+{
+    public static void search(int arr[],int first, int last, int key)
+    {
+        int mid=(first +last)/2;
+        while(first<=last)
+        {
+            if(arr[mid]<key)
+            {
+                first=mid+1;
+            }
+            else if(arr[mid]==key)
+            {
+                System.out.println("Element found at "+mid);
+                break;
+            }
+            else{
+                last=mid-1;
+            }
+            mid=(first+last)/2;
+        }
+        if(first>last)
+        {
+            System.out.println("Not found");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int key=20;
+        int len=arr.length-1
+        System.out.println(key+" Key Found at position "+search(arr,0,len,key));
+    }
